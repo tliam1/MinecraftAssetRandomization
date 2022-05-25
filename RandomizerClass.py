8758497bff4dbf48a6ced68a4b34bc9100e1c9ac
CosmicShiny <gdshiny@gmail.com>
2022-05-25 13:58:43 -1000

    Added a funciton that writes the names of the files to a text document

--- RandomizerClass.py
+++ RandomizerClass.py
@@ -1,8 +1,10 @@
-class randomizer:
+
+class Randomizer:
+
     def __init__(self, directory, files):  # I included the directory so we dont have to keep stating it
         self.directory = directory
         self.files = files
 
-    def randomizeFileNames(self):
+    def randomize_file_names(self):
         # randomize list and make if's for exclusions
         print("Testing function", self.files)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from os import listdir
 from os.path import isfile, join
-from RandomizerClass import randomizer
+from RandomizerClass import Randomizer
 
 
 def grab_files_from_location(directoryname):
@@ -10,7 +10,7 @@ def grab_files_from_location(directoryname):
 
 
 mypath = os.path.dirname(os.path.realpath(__file__))
-print(mypath)
+# print(mypath)
 onlyfiles = []
 
 # onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
@@ -22,9 +22,20 @@ onlyfiles = []
 
 
 # onlyfiles += grab_files_from_location(mypath + "\\TestFolder")
-# testFolderRandomizerClass is a class that holds all the info & files for a specific folder. Keeps things small and organized
-testFolderRandomizerClass = randomizer(mypath + "\\TestFolder", grab_files_from_location(mypath + "\\TestFolder"))
+# testFolderRandomizerClass is a class that holds all the info &
+# files for a specific folder. Keeps things small and organized
+testFolderRandomizerClass = Randomizer(mypath + "\\TestFolder", grab_files_from_location(mypath + "\\TestFolder"))
 print(testFolderRandomizerClass.files)
-testFolderRandomizerClass.randomizeFileNames()  # just testing the class method
+testFolderRandomizerClass.randomize_file_names()  # just testing the class method
+
+# Below just takes testFolderRandomizerClass.files and adds it to a file in randomizer_name_storage,
+# I don't really know how to orgnaize all of this yet really, so it is stil to be determined if I'm just going
+# to put this into a function, put it in a class, etc.
+# Also, for now it is taking the file names from TestFolder.
 
 
+def store_file_names():
+    testfile = open(mypath + "\\randomizer_name_storage" + "\\TestFile.txt", "w")
+
+    for file in testFolderRandomizerClass.files:
+        testfile.write(file + "\n")
