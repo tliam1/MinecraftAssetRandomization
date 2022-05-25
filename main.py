8ad330c2132ff7a312f06ef2f425b11148500a9b
tliam1 <tliam@hawaii.edu>
2022-05-24 17:53:12 -1000

    Grabbing Files

--- main.py
+++ main.py
@@ -4,19 +4,22 @@ from os.path import isfile, join
 
 
 def grab_files_from_location(directoryname):
-    print(directoryname)
+    filesList = [f for f in listdir(directoryname) if isfile(join(directoryname, f))]
+    return filesList
 
 
 mypath = os.path.dirname(os.path.realpath(__file__))
 print(mypath)
 onlyfiles = []
+
 # onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-for f in listdir(mypath):
-    if isfile(join(mypath, f)):
-        f = f.split(".")[0]
-        onlyfiles.append(f)
+# for f in listdir(mypath + "\\TestFolder"):
+#     if isfile(join(mypath + "\\TestFolder", f)):
+#         f = f.split(".")[0]
+#         onlyfiles.append(f)
+#         print(f)
 
 
-# mypath = grabFilesFromLocation()
+onlyfiles += grab_files_from_location(mypath + "\\TestFolder")
 
 print(onlyfiles)
