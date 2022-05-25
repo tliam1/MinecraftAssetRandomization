42b885ee2359e058d53d9f9fca4c2765d7d5890b
CosmicShiny <gdshiny@gmail.com>
2022-05-24 17:43:21 -1000

    Balls 3

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import os
 from os import listdir
 from os.path import isfile, join
 
-def grabFilesFromLocation(directoryName):
-    print(directoryName)
+
+def grab_files_from_location(directoryname):
+    print(directoryname)
 
 
 mypath = os.path.dirname(os.path.realpath(__file__))
@@ -15,10 +16,7 @@ for f in listdir(mypath):
         f = f.split(".")[0]
         onlyfiles.append(f)
 
-#mypath = grabFilesFromLocation()
+
+# mypath = grabFilesFromLocation()
+
 print(onlyfiles)
-print("Balls")
-
-print("Balls")
-
-print("Balls")
