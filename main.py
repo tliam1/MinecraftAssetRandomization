7f9809087dde08937ffc6c38df32b8361360e34d
tliam1 <tliam@hawaii.edu>
2022-05-24 18:27:21 -1000

    Grabbing Files

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def grab_files_from_location(directoryname):
 
 
 mypath = os.path.dirname(os.path.realpath(__file__))
-print("piss")
+print(mypath)
 onlyfiles = []
 
 # onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
