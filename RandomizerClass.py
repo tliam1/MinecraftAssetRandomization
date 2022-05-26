c2f8d0c55979a38f09df32b0263812bc38cf971b
tliam1 <tliam@hawaii.edu>
2022-05-25 14:05:35 -1000

    RandomizedList

--- RandomizerClass.py
+++ RandomizerClass.py
@@ -1,10 +1,10 @@
-
+import random
 class Randomizer:
-
     def __init__(self, directory, files):  # I included the directory so we dont have to keep stating it
         self.directory = directory
         self.files = files
 
     def randomize_file_names(self):
         # randomize list and make if's for exclusions
+        random.shuffle(self.files)
         print("Testing function", self.files)
