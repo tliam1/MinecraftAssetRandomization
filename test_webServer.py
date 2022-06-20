661f3e1125cf6c7d3b869b41787d0bc27902f6c2
tliam1 <tliam@hawaii.edu>
2022-06-19 16:21:30 -1000

    Working on Navbar

--- test_webServer.py
+++ test_webServer.py
@@ -4,15 +4,16 @@ from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 # displays what will be on the home page
-
+# Get = insecure way of getting info
+# Post = secure way of getting info from user
 # routes this to be the default home page
 @app.route("/")
 def Home_Page():
     return render_template("index.html", content=["Dream", "SnapNap", "George Not Found"])
 
-@app.route("/admin")
+@app.route("/About_Us")
 def admin():  # could be used for button redirections later on
-    return redirect(url_for("Home_Page"))
+    return "About Us!"
 
 # anything typed after / will be given as the name and displayed on a new page
 # @app.route("/<name>")  # can be passed as a parameter
