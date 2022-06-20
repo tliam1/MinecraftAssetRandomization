0dfa59601ce641c1ce5eb92bbea4e91480d17ac7
tliam1 <tliam@hawaii.edu>
2022-06-19 15:53:32 -1000

    Inheritance with html

--- test_webServer.py
+++ test_webServer.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 #  pip install flask to get the stuff for this
 
 app = Flask(__name__)
@@ -8,20 +8,19 @@ app = Flask(__name__)
 # routes this to be the default home page
 @app.route("/")
 def Home_Page():
-    return " <h1>MineCraft Asset Randomizer<h1> Hello! This is the main page"
-# time to learn HTML formatting hell
+    return render_template("index.html", content=["Dream", "SnapNap", "George Not Found"])
 
 @app.route("/admin")
 def admin():  # could be used for button redirections later on
     return redirect(url_for("Home_Page"))
 
 # anything typed after / will be given as the name and displayed on a new page
-@app.route("/<name>")  # can be passed as a parameter
-def user(name):
-    return f"Welcome {name}"
+# @app.route("/<name>")  # can be passed as a parameter
+# def user(name):
+#     return f"Welcome {name}"
 
 
 # still need to learn why
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
