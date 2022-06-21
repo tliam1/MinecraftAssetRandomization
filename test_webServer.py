d6bbbb54c2a5e625d60e9aef24140b0f3a825a9b
tliam1 <tliam@hawaii.edu>
2022-06-20 19:07:56 -1000

    removed old test folder and updated website for downloads and Product Information

--- test_webServer.py
+++ test_webServer.py
@@ -1,7 +1,7 @@
 from flask import Flask, redirect, url_for, render_template, request
 from flask_dropzone import Dropzone
 import os
-
+from main import start_program
 #  pip install Flask-Dropzone
 #  pip install flask to get the stuff for this
 folderDir = ""
@@ -21,7 +21,7 @@ dropzone = Dropzone(app)
 # routes this to be the default home page
 @app.route("/")
 def Home_Page():
-    return render_template("index.html", content=["\n", "\n", "\n", "\n", "\n", "\n", "\n", "\n", "\n", "\n", "\n", "\n", "\n", "\n", "\n", "\n", "\n"])
+    return render_template("index.html", content=[])
 
 
 @app.route("/About_Us")
@@ -33,22 +33,16 @@ def About_Us():
 def Randomize():
     # randomize before this point or the death of all of us begins
     if request.method == "POST":
-        # when pressing submit do something
-        temp = request.form["asset_dir"]
-        listOfGlobals = globals()  # only way to assign new values to "Undetermined value" globals
-        listOfGlobals['folderDir'] = temp
-        print(folderDir)
-        return redirect(url_for("Randomizer_Success"))
+        return render_template("randomize.html")
     else:
         return render_template("randomize.html")
 
-
-@app.route("/Successful_Randomization")
-def Randomizer_Success():
-    # move file to new location (I.E. out of project location)
-    return f"<h1>Randomization Sucessful! You can now close this page!</h1>\n" \
-           f"<p>Base Assets Folder Location: {folderDir}</p>\n" \
-           f"<p>Randomized Assets Folder Location: enter Dir here\n"
+# background process happening without any refreshing
+@app.route('/Randomize/<Ver_Name>')
+def background_process_randomize(Ver_Name):
+    if Ver_Name == "1.19":
+        start_program()
+    return render_template("Download.html")
 
 
 # still need to learn why
